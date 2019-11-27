55c55ca0876fbd5e29f1e265935bd1f13004d14b
Edward <957196958@qq.com>
2019-11-27 18:17:40 +0800

    好友上下线通知,消息待完善

--- src/modules/socket/socket.gateway.ts
+++ src/modules/socket/socket.gateway.ts
@@ -2,6 +2,7 @@ import { SubscribeMessage, WebSocketGateway, WebSocketServer, OnGatewayInit } fr
 import { UserService } from '../user/user.service';
 import { UserStatus } from '../../core/interfaces/enums/user-status.enum';
 import { RedisService } from 'nestjs-redis';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 let num = 0;
 @WebSocketGateway()
@@ -11,7 +12,7 @@ export class SocketGateway implements OnGatewayInit {
     private redisService: RedisService
   ) { }
   afterInit(server) {
-    console.log(server)
+    // console.log(server)
   }
   handleConnection(client) {
     // console.log('ClientId:'+client.id+'进入')
@@ -20,8 +21,8 @@ export class SocketGateway implements OnGatewayInit {
     // console.log('ClientId:'+client.id+'离开')
   }
 
-  users = [];
   @WebSocketServer() server;
+
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any) {
     num++;
@@ -67,43 +68,62 @@ export class SocketGateway implements OnGatewayInit {
     if (online && online.length > 0) {
       for (let item of online) {
         // client.to(item.socketId).emit('friends-on',{ id, socketId: client.id})
-        let refreshFriendsList = await this.getOnlineFriendsList(item.id); 
-        client.to(item.socketId).emit('friends-fresh', refreshFriendsList)
+        let refreshFriendsList = await this.getOnlineFriendsList(item.id);
+        client.to(item.socketId).emit('friends_fresh', refreshFriendsList)
       }
     }
     console.log(`SocketID:${client.id},ID:${payload.id}--登录成功`)
 
-    client.on('disconnecting', async() => {
+    client.on('disconnecting', async () => {
+      console.log(`SocketID:${client.id},ID:${payload.id}--即将退出....`)
       this.userMap.delete(id);
-      if (online && online.length > 0) {
-        for (let item of online) {
-          // client.to(item.socketId).emit('friends-off',{ id, socketId: client.id})
-          let refreshFriendsList = await this.getOnlineFriendsList(item.id); 
-          client.to(item.socketId).emit('friends-fresh', refreshFriendsList)
+      let online2 = [];
+      friendsIds.map(item => {
+        if (this.userMap.has(item)) {
+          // this.onlineMap.set(item,this.userMap.get(item))
+          online2.push({ id: item, socketId: this.userMap.get(item) })
+        }
+      })
+      if (online2 && online2.length > 0) {
+        for (let item of online2) {
+          let refreshFriendsList = await this.getOnlineFriendsList(item.id);
+          client.to(item.socketId).emit('friends_fresh', refreshFriendsList)
         }
       }
-      console.log(`SocketID:${client.id},ID:${payload.id}--退出成功`)
+      console.log(`SocketID:${client.id},ID:${payload.id}--退出成功....`)
     })
-    console.log({ online })
     return {
       event: "online",
-      data: {
-        online
-      }
+      data: online
     };
   }
 
   //根据用户id获取 用户 在线好友列表
-  async getOnlineFriendsList(id){
+  async getOnlineFriendsList(id) {
 
     let friendsIds = await this.redisService.getClient().smembers(id);
     let online = [];
     friendsIds.map(item => {
       if (this.userMap.has(item)) {
-        // this.onlineMap.set(item,this.userMap.get(item))
         online.push({ id: item, socketId: this.userMap.get(item) })
       }
     })
     return online;
+  }
+
+  @SubscribeMessage('chat')
+  async chatTo(client, payload) {
+    console.log(this.userMap)
+    let toSocketId = this.userMap.get(payload.to)
+    console.log({
+      from: client.id,
+      to: toSocketId,
+      message: payload.message
+    })
+    await client.to(toSocketId).emit('chat_to', { from: payload.from, to:payload.to, message: payload.message });
+    return {
+      event:'chat_status',
+      data:'ok'
+    };
   }
 }
